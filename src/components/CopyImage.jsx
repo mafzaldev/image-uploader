043e2dec10ceb968fb13ff8c1982e4684f6a491a
Afzal <afzalashraf814@gmail.com>
2023-01-13 21:33:22 +0500

    fixed deployment issues

--- src/components/CopyImage.jsx
+++ src/components/CopyImage.jsx
@@ -1,29 +1,20 @@
 import React, { useState } from "react";
 import Button from "./Button";
 import tick from "../assets/tick.svg";
-import { API_URL } from "../utils";
 
-const CopyImage = ({ data }) => {
+const CopyImage = ({ imageURL }) => {
   return (
     <>
       <img src={tick} alt="tick" />
       <h1 className="font-medium text-[#4F4F4F]">Uploaded Successfully!</h1>
-      <div className="h-56 w-80 bg-[#F6F8FB] border-[1px] border-dashed border-[#97BEF4] rounded-xl">
-        <img
-          src={`${API_URL}/${data.imageURL}`}
-          alt=""
-          className="h-56 w-80 object-contain"
-        />
+      <div className="h-56 bg-[#F6F8FB] rounded-xl">
+        <img src={imageURL} alt="" className="h-56 object-contain" />
       </div>
       <div className="w-80 h-9 flex justify-center items-center border-[1px] border-solid border-[#E0E0E0] rounded-lg truncate">
         <span className="w-56 text-xs font-medium text-[#4F4F4F] truncate">
-          {`${API_URL}/${data.imageURL}`}
+          {imageURL}
         </span>
-        <Button
-          onClick={() =>
-            navigator.clipboard.writeText(`${API_URL}/${data.imageURL}`)
-          }
-        >
+        <Button onClick={() => navigator.clipboard.writeText(imageURL)}>
           Copy Link
         </Button>
       </div>
